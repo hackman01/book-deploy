e8900843bbc3bd5e5ef71fa04e080e0c48fddd09
hackma01 <amanmourya295@gmail.com>
2025-02-03 11:12:50 +0530

    updated port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 8000
 
 const bookRouter = require('./src/routes/book.route')
 const requestRouter = require('./src/routes/request.route')
